ff69896b752e56587f4413f77cee02b69d43c6a6
Rusty Shackleford <gigolo@hotmail.co.uk>
2017-06-25 16:10:58 +0100

    Initial Commit

---
+++ cee-lo/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  cee-lo
+//
+//  Created by George Watson on 25/06/2017.
+//  Copyright © 2017 George Watson. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+	// insert code here...
+	printf("Hello, World!\n");
+	return 0;
+}
